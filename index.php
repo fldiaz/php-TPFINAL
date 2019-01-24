ba2a946a6a360c940dbc9429c27a9e4e7e1bbb94
fldiaz <45696218+fldiaz@users.noreply.github.com>
2019-01-24 14:20:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -21,21 +21,11 @@ $appName = explode('.', $domain)[0];
     </head>
 
     <body>
-        <header class="navbar">
-            <a class="logo" href="#"><img src="/getting-started/logo-developers.png" alt=""></a>
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a target="_blank" href="http://developers.mercadolibre.com/getting-started/">Getting Started</a></li>
-                    <li><a target="_blank" href="http://developers.mercadolibre.com/api-docs/">API Docs</a></li>
-                    <li><a target="_blank" href="http://developers.mercadolibre.com/community/">Community</a></li>
-                </ul>
-            </nav>
-        </header>
 
         <div class="header">
             <div>
-                <h1>Getting Started with Mercado Libre's PHP SDK</h1>
-                <h2>Official PHP SDK for Mercado Libre's API.</h2>
+                <h1>APLICACION DE PRUBA</h1>
+                <h2>TRABAJO FINAL ESPECIALIZACION EN CIENCIA DE DATOS</h2>
             </div>
         </div>
 
@@ -71,10 +61,6 @@ $appName = explode('.', $domain)[0];
                 <h3>Examples</h3>
                 <p>
                     Check the following examples, they will help you to start working with our API!
-                </p>
-                <p>
-                    Note that these examples work for MLB(Brasil) by default. If you'd like to try them in your own country, please, <a href="https://github.com/mercadolibre/php-sdk/blob/master/configApp.php#L16">update this line</a> in your project, with
-                    your own <b>$site_id</b> before executing them.
                 </p>
             </div>
             <hr>
@@ -118,48 +104,11 @@ $appName = explode('.', $domain)[0];
                     ?>
 
                 </div>
-                <div class="col-sm-6 col-md-6">
-                    <h3>Get site</h3>
-                    <p>Make a simple GET to <a href="https://api.mercadolibre.com/sites">sites resource</a> with your <b>$site_id</b> to obtain information about a a site. Like id, name, currencies, categories, and other settings.</p>
-                    <p><a class="btn" href="../examples/example_get.php">GET</a></p>
-                </div>
-            </div>
-            <hr>
-            <div class="row">
-                <div class="col-md-6">
-                    <h3>Publish an Item</h3>
-                    <p>
-                        This is a example of how to list an item in <b>MLB</b> (Brasil).
-                       <br /> <b>You need to be authenticated to make it work.</b>
-                       <br /> To be able to list an item in another country, <a href="https://github.com/mercadolibre/php-sdk/blob/master/examples/example_list_item.php">please update this file</a>, with values according to the site Id where your app works, like <b>category_id</b> and <b>currency</b>.
-                     <br />
-                    </p>
-                    <pre class="pre-item">
-"title" => "Item De Teste - Por Favor, Não Ofertar! --kc:off",
-        "category_id" => "MLB1227",
-        "price" => 10,
-        "currency_id" => "BRL",
-        "available_quantity" => 1,
-        "buying_mode" => "buy_it_now",
-        "listing_type_id" => "bronze",
-        "condition" => "new",
-        "description" => "Item de Teste. Mercado Livre's PHP SDK.",
-        "video_id" => "RXWn6kftTHY",
-        "warranty" => "12 month",
-        "pictures" => array(
-            array(
-                "source" => "https://upload.wikimedia.org/wikipedia/commons/f/fd/Ray_Ban_Original_Wayfarer.jpg"
-            ),
-            array(
-                "source" => "https://upload.wikimedia.org/wikipedia/commons/a/ab/Teashades.gif"
-            )
-        )
-    )
-                    </pre>
-
+   
                     <?php
+  
                     $meli = new Meli($appId, $secretKey);
-                    if($_GET['code'] && $_GET['publish_item']) {
+                    if($_GET['code']  {
                         // If the code was in get parameter we authorize
                         $user = $meli->authorize($_GET['code'], $redirectURI);
                         // Now we create the sessions with the authenticated user
@@ -174,77 +123,28 @@ $appName = explode('.', $domain)[0];
                                 echo "Exception: ",  $e->getMessage(), "\n";
                             }
                         }
-                        // We construct the item to POST
-                        $item = array(
-                            "title" => "Item De Teste - Por Favor, Não Ofertar! --kc:off",
-        "category_id" => "MLB1227",
-        "price" => 10,
-        "currency_id" => "BRL",
-        "available_quantity" => 1,
-        "buying_mode" => "buy_it_now",
-        "listing_type_id" => "bronze",
-        "condition" => "new",
-        "description" => "Item de Teste. Mercado Livre's PHP SDK.",
-        "video_id" => "RXWn6kftTHY",
-        "warranty" => "12 month",
-        "pictures" => array(
-            array(
-                "source" => "https://upload.wikimedia.org/wikipedia/commons/f/fd/Ray_Ban_Original_Wayfarer.jpg"
-            ),
-            array(
-                "source" => "https://upload.wikimedia.org/wikipedia/commons/a/ab/Teashades.gif"
-            )
-        )
-    );
-                        
-                        $response = $meli->post('/items', $item, array('access_token' => $_SESSION['access_token']));
-                        // We call the post request to list a item
-                        echo "<h4>Response</h4>";
-                        echo '<pre class="pre-item">';
-                        print_r ($response);
+
+                    <?php
+                    $meli = new Meli($appId, $secretKey);
+                    if($_GET['code']) {
+                        // If the code was in get parameter we authorize
+                        $user = $meli->authorize($_GET['code'], $redirectURI);
+                        // Now we create the sessions with the authenticated user
+                        $_SESSION['access_token'] = $user['body']->access_token;
+                        $_SESSION['expires_in'] = $user['body']->expires_in;
+                        $_SESSION['refresh_token'] = $user['body']->refresh_token;
+                        // We can check if the access token in invalid checking the time
+                        if($_SESSION['expires_in'] + time() + 1 < time()) {
+                            try {
+                                print_r($meli->refreshAccessToken());
+                            } catch (Exception $e) {
+                                echo "Exception: ",  $e->getMessage(), "\n";
+                            }
+                        }
+                        $params = array();
+                        $url = '/sites/' . $siteId;
+                        $result = $meli->get($url, $params, array('access_token' => $_SESSION['access_token']));;
+                        echo '<pre>';
+                        print_r($result);
                         echo '</pre>';
-                        echo "<h4>Success! Your test item was listed!</h4>";
-                        echo "<p>Go to the permalink to see how it's looking in our site.</p>";
-                        echo '<a target="_blank" href="'.$response["body"]->permalink.'">'.$response["body"]->permalink.'</a><br />';
-                    } else if($_GET['code']) {
-                        echo '<p><a alt="Publish Item" class="btn" href="/?code='.$_GET['code'].'&publish_item=ok">Publish Item</a></p>';
-                    } else {
-                        echo '<p><a alt="Publish Item" class="btn disable" href="#">Publish Item</a> </p>';
-                    }
-                    ?>
-
-                </div>
-
-                <div class="col-md-6">
-                    <h3>Get started!</h3>
-                    <p>Now you know how easy it is to get information from our API. Check the rest of the examples on the SDK, and modify them as you like in order to List an item, update it, and other actions.</p>
-                    <p><a class="btn" href="https://github.com/mercadolibre/php-sdk/tree/master/examples">More examples</a></p>
-                </div>
-            </div>
-
-            <hr>
-
-            <div class="row">
-                <h3>Your Credentials</h3>
-                <div class="row-info col-sm-3 col-md-3">
-                    <b>App_Id: </b>
-                    <?php echo $appId; ?>
-                </div>
-                <div class="row-info col-sm-3 col-md-3">
-                    <b>Secret_Key: </b>
-                    <?php echo $secretKey; ?>
-                </div>
-                <div class="row-info col-sm-3 col-md-3">
-                    <b>Redirect_URI: </b>
-                    <?php echo $redirectURI; ?>
-                </div>
-                <div class="row-info col-sm-3 col-md-3">
-                    <b>Site_Id: </b>
-                    <?php echo $siteId; ?>
-                </div>
-            </div>
-            <hr>
-        </main>
-    </body>
-
-    </html>
+                       
