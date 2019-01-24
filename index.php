477c386f63eb0cec47d67fccc3fa9ad67eeb56b0
fldiaz <45696218+fldiaz@users.noreply.github.com>
2019-01-24 10:55:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -128,16 +128,6 @@ $appName = explode('.', $domain)[0];
                     <h3>Get site</h3>
                     <p>Make a simple GET to <a href="https://api.mercadolibre.com/sites">sites resource</a> with your <b>$site_id</b> to obtain information about a a site. Like id, name, currencies, categories, and other settings.</p>
                     <p><a class="btn" href="../examples/example_get.php">GET</a></p>
-                         <?php
-require '../Meli/meli.php';
-require '../configApp.php';
-$meli = new Meli($appId, $secretKey);
-$params = array();
-$url = '/sites/' . $siteId;
-$result = $meli->get($url, $params);
-echo '<pre>';
-print_r($result);
-echo '</pre>';
                 </div>
             </div>
             <hr>
