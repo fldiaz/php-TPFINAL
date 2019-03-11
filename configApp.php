22fb70a88fde4dffdaac17fab70a150017d73a63
fldiaz <45696218+fldiaz@users.noreply.github.com>
2019-03-11 16:47:15 -0300

    Update configApp.php

--- configApp.php
+++ configApp.php
@@ -15,14 +15,14 @@ $redirectURI = getenv('Redirect_URI');
 If you don't know your site_id go to our sites resources: https://api.mercadolibre.com/sites  */
 $siteId = 'MLA';
 
-/* Base de datos.*/
+/* Base de datos.
 $db_handle = getenv('DATABASE_URL')
 $db_query = "SELECT token, refresh_token";
 $db_result = pg_query($db_query);
 $db_myrow = pg_fetch_assoc($db_result);
     //Get tokens
     $token = $db_myrow[token];﻿
-    $refresh_token = $db_myrow[refresh_token];
+    $refresh_token = $db_myrow[refresh_token];*/
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //If you don't use Heroku use the next config
