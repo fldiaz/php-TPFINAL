76b7950d5d8c8088ac8575aa6f003ab8076bf116
fldiaz <45696218+fldiaz@users.noreply.github.com>
2019-04-25 11:18:09 -0300

    saco
    			"$acceso"=>"'". proteger_cadena($_GET["access_token"]). "'",
    			"$expira"=>"'". proteger_cadena(($_GET["expires_in"]+time())). "'",
                            "$reacceso"=>"'". proteger_cadena(($_GET["refresh_token"];
                                
                            
                            $conn_string = "host=ec2-54-225-121-235.compute-1.amazonaws.com port=5432 dbname=d8dcgc3t7r73g4 user=xjubydasnrqhgz password=30500059e50b99a49091dc7c5629414026f4dc5127988245a6f8a668882d242a";
    
                            // establecemos una conexion con el servidor postgresSQL
                            $dbconn = pg_connect($conn_string);
    
                            // Revisamos el estado de la conexion en caso de errores. 
                            if(!$dbconn) {
                            echo "Error: No se ha podido conectar a la base de datos\n";
                            } else {
                            ////////////////////////////////
                            //hacer el insert $_SESSION['access_token']; $_SESSION['expires_in'];$_SESSION['refresh_token']
                            
                            $query = "INSERT INTO usuarios VALUES (token, expiration, refresh) values ('". $acceso ."','".$expira."','". $reacceso."');";
                            $result = pg_query($query); 
                            
                            /////////////// para probar errores en insert
                           
                            echo($querry);
                            ///////////// fin para probar errores en insert
                            }
                            pg_close($dbconn);

--- index.php
+++ index.php
@@ -84,67 +84,13 @@ $appName = explode('.', $domain)[0];
                             }
                         }
                         $user = $meli->get('/users/me', array('access_token' => $access_token));
-                       
-			"$acceso"=>"'". proteger_cadena($_GET["access_token"]). "'",
-			"$expira"=>"'". proteger_cadena(($_GET["expires_in"]+time())). "'",
-                        "$reacceso"=>"'". proteger_cadena(($_GET["refresh_token"];
-                            
-                        
-                        $conn_string = "host=ec2-54-225-121-235.compute-1.amazonaws.com port=5432 dbname=d8dcgc3t7r73g4 user=xjubydasnrqhgz password=30500059e50b99a49091dc7c5629414026f4dc5127988245a6f8a668882d242a";
-
-                        // establecemos una conexion con el servidor postgresSQL
-                        $dbconn = pg_connect($conn_string);
-
-                        // Revisamos el estado de la conexion en caso de errores. 
-                        if(!$dbconn) {
-                        echo "Error: No se ha podido conectar a la base de datos\n";
-                        } else {
-                        ////////////////////////////////
-                        //hacer el insert $_SESSION['access_token']; $_SESSION['expires_in'];$_SESSION['refresh_token']
-                        
-                        $query = "INSERT INTO usuarios VALUES (token, expiration, refresh) values ('". $acceso ."','".$expira."','". $reacceso."');";
-                        $result = pg_query($query); 
-                        
-                        /////////////// para probar errores en insert
-                       
-                        echo($querry);
-                        ///////////// fin para probar errores en insert
-                        }
-                        pg_close($dbconn);
                         echo '<pre>';
                             print_r($_SESSION);
                         echo '</pre>';
                     } else {
                         echo '<p><a alt="Login using MercadoLibre oAuth 2.0" class="btn" href="' . $meli->getAuthUrl($redirectURI, Meli::$AUTH_URL[$siteId]) . '">OK</a></p>';
-                        "$acceso"=>"'". proteger_cadena($_GET["access_token"]). "'",
-			"$expira"=>"'". proteger_cadena(($_GET["expires_in"]+time())). "'",
-                        "$reacceso"=>"'". proteger_cadena(($_GET["refresh_token"];
-                            
-                        
-                        $conn_string = "host=ec2-54-225-121-235.compute-1.amazonaws.com port=5432 dbname=d8dcgc3t7r73g4 user=xjubydasnrqhgz password=30500059e50b99a49091dc7c5629414026f4dc5127988245a6f8a668882d242a";
-
-                        // establecemos una conexion con el servidor postgresSQL
-                        $dbconn = pg_connect($conn_string);
-
-                        // Revisamos el estado de la conexion en caso de errores. 
-                        if(!$dbconn) {
-                        echo "Error: No se ha podido conectar a la base de datos\n";
-                        } else {
-                        ////////////////////////////////
-                        //hacer el insert $_SESSION['access_token']; $_SESSION['expires_in'];$_SESSION['refresh_token']
-                        
-                        $query = "INSERT INTO usuarios VALUES (token, expiration, refresh) values ('". $acceso ."','".$expira."','". $reacceso."');";
-                        $result = pg_query($query); 
-                        
-                        /////////////// para probar errores en insert
-                       
-                        echo($querry);
-                        ///////////// fin para probar errores en insert
-                        }
-                        pg_close($dbconn);
                     }
                     ?>
-            
                 </div>
             </div>
             <hr>
